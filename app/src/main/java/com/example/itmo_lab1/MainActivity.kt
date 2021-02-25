e58c9f1dfc6ade2f0db2360d18bf4d15f4dbe12d
Antondstd <45855111+Antondstd@users.noreply.github.com>
2021-02-25 03:32:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/itmo_lab1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.itmo_lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_four)
+    }
+}
